f56eb3d44fd49daad79aa5dd98e86d098b4455b0
mitaryuto <mitaryuto@gmail.com>
2022-09-25 00:04:57 +0900

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>KAN GANA SPACE</title>
+</head>
+<body>
+ <h1>KAN GANA SPACE</h1>
+</body>
+</html>
